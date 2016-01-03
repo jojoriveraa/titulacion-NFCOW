2b778e657a1c20a4a5cbf2ff7aa6d9dc39c6a5a6
jojoriveraa <jojoriveraa@gmail.com>
2016-01-03 01:40:23 -0600

    Redirigir del formulario de productos al carrito de compras

--- NFCow/products/views.py
+++ NFCow/products/views.py
@@ -1,16 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth import login
 
 from .forms import ProductForm
 from .models import Product
 
 # Create your views here.
 def product_detail(request, id):
-	form = ProductForm(request.POST or None)
-	
-	product = get_object_or_404(Product, id = id)
-	
-	# if form.is_valid():
-	# 	form.save()
-	
-	return render(request, 'product.html', {'form' : form, 'product' : product})
+	if request.method == 'POST':
+		# if form.is_valid():
+		# 	form.save()
+		return HttpResponseRedirect('/shopping-cart/1')
+	else:
+		form = ProductForm(request.POST or None)
+		product = get_object_or_404(Product, id = id)
+		return render(request, 'product.html', {'form' : form, 'product' : product})
