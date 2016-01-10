60e890564e44c9596d04bb69f1058c5834d8f071
jojoriveraa <jojoriveraa@gmail.com>
2016-01-10 00:14:49 -0600

    Avatar en el menú contextual

--- NFCow/NFCow/context_processors.py
+++ NFCow/NFCow/context_processors.py
@@ -1,5 +1,7 @@
 from userprofiles.models import CustomerProfile
 
 def customer_avatar(request):
+	if request.user.is_anonymous():
+			return {'avatar' : None }
 	customer = CustomerProfile.objects.filter(user__username = request.user.username)[0]
 	return {'avatar' : customer.avatar_url}
