a04d21ac12eb18d904d98386faf2019ab53ccec1
jojoriveraa <jojoriveraa@gmail.com>
2016-01-10 01:56:58 -0600

    Solucionar bugs SignUp

--- NFCow/NFCow/context_processors.py
+++ NFCow/NFCow/context_processors.py
@@ -3,6 +3,12 @@ from userprofiles.models import CustomerProfile
 def customer_avatar(request):
 	if request.user.is_anonymous():
 		return {'avatar' : None }
+	
+	q_customer = CustomerProfile.objects.filter(user = request.user)
+	if not q_customer:
+		q_customer = CustomerProfile.objects.create(user = request.user)
+
 		
 	customer = CustomerProfile.objects.filter(user__username = request.user.username)[0]
+	
 	return {'avatar' : customer.avatar}
